6c40149247aacbb08d1da2812275906b930df286
Flowrt <90101574+RAELIE1@users.noreply.github.com>
2025-08-16 22:12:17 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,11 @@ use tokio::task;
 mod templates;
 
 // Allowed origins
-static ALLOWED_ORIGINS: Lazy<[&str; 3]> = Lazy::new(|| [
+static ALLOWED_ORIGINS: Lazy<[&str; 4]> = Lazy::new(|| [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://high-bluds-tv.vercel.app/"
+    "https://high-bluds-tv.vercel.app/",
+    "https://anime-mapper-woad.vercel.app",
 ]);
 
 // Reqwest client pool
